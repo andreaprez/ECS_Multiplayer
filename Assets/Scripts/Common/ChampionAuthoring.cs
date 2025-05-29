b1ef90d15ac97cdb51a3049061c4c20f5cce4356
Andrea <andreaprezp@gmail.com>
2025-05-29 22:53:40 +0200

    Implement components for Champion

---
+++ Assets/Scripts/Common/ChampionAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ECS_Multiplayer.Common
+{
+    public class ChampionAuthoring : MonoBehaviour
+    {
+        public class ChampionBaker : Baker<ChampionAuthoring>
+        {
+            public override void Bake(ChampionAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<ChampionTag>(entity);
+                AddComponent<NewChampionTag>(entity);
+                AddComponent<GameTeam>(entity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Common/ChampionComponents.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+
+namespace ECS_Multiplayer.Common
+{
+    public struct ChampionTag : IComponentData { }
+    public struct NewChampionTag : IComponentData { }
+    public struct GameTeam : IComponentData
+    {
+        [GhostField] public TeamType Value;
+    }
+}
